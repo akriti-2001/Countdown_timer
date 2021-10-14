19526f517ef39f960f6b8c8c85e080d7226eff64
Akriti sinha <akritisinha2001@gmail.com>
2021-10-14 22:24:58 +0530

    My project

---
+++ script.js
@@ -0,0 +1,22 @@
+var deadline = new Date("jan 01, 2022 15:37:25").getTime();
+
+var x = setInterval(function() {
+
+var now = new Date().getTime();
+var t = deadline - now;
+var days = Math.floor(t / (1000 * 60 * 60 * 24));
+var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((t % (1000 * 60)) / 1000);
+document.getElementById("days").innerHTML =days ;
+document.getElementById("hours").innerHTML =hours;
+document.getElementById("minutes").innerHTML = minutes;
+document.getElementById("seconds").innerHTML =seconds;
+if (t < 0) {
+		clearInterval(x);
+		document.getElementById("demo").innerHTML = "HAPPY   NEW   YEAR!!";
+		document.getElementById("days").innerHTML ='0';
+		document.getElementById("hours").innerHTML ='0';
+		document.getElementById("minutes").innerHTML ='0' ;
+		document.getElementById("seconds").innerHTML = '0'; }
+}, 1000);
